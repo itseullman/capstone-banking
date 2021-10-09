b7271c3b20fba7cbc4f5325b8b94ed69d23c4bba
gitFloyd <dfincher@kent.edu>
2021-10-08 21:18:33 -0400

    Implemented action=table
    The page=data now has action=table implemented. This page is just a test.

--- modules/data.php
+++ modules/data.php
@@ -1,19 +1,31 @@
 <?php
 
-// create curl resource
-$ch = curl_init();
 
-// set url
-curl_setopt($ch, CURLOPT_URL, OAKS_WWW);
+function get_oaks_data() {
+	$result = file_get_contents(OAKS_WWW, false, stream_context_create(['http' => ['method'  => 'GET']]));
+	$result = json_decode($result, true);
+	return $result;
+}
 
-//return the transfer as a string
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-// $output contains the output string
-$data = curl_exec($ch);
-
-// close curl resource to free up system resources
-curl_close($ch);     
+/*
+  object(stdClass)#1 (7) {
+    ["id"]=>
+    string(5) "16621"
+    ["title"]=>
+    string(83) "A scalable associative processor with applications in database and image processing"
+    ["author"]=>
+    string(44) "Hong Wang;Lei Xie;Meiduo Wu;Robert A. Walker"
+    ["date"]=>
+    string(7) "2004-04"
+    ["public_url"]=>
+    string(32) "https://oaks.kent.edu/node/16621"
+    ["pdf_url"]=>
+    string(0) ""
+    ["search_api_excerpt"]=>
+    string(0) ""
+  }
+*/
 
 switch (REQUEST_ACTION) {
 	case 'all':
@@ -21,10 +33,85 @@ switch (REQUEST_ACTION) {
 	break;
 	
 	case 'table':
-	echo $data;
+	case_data_table();
 	break;
 	
 	default:
 	echo "case 'default' not implemented yet.";
 	break;
 }
+
+
+
+
+function case_data_table() {
+	$data = get_oaks_data();
+	$output = '';
+	if (is_array($data)) {
+		foreach ($data as $item) {
+			$output .= <<<EOT
+	<tr>
+		<td>
+		{$item['title']}
+		</td>
+		<td>
+		{$item['author']}
+		</td>
+		<td>
+		{$item['date']}
+		</td>
+		<td>
+		{$item['public_url']}
+		</td>
+		<td>
+		{$item['pdf_url']}
+		</td>
+	</tr>
+EOT;
+		}
+	}
+	
+	echo <<<EOT
+<table>
+	<tr>
+		<th>
+		Title
+		</th>
+		<th>
+		Author
+		</th>
+		<th>
+		Date
+		</th>
+		<th>
+		Public Url
+		</th>
+		<th>
+		PDF Url
+		</th>
+	</tr>
+$output
+</table>
+EOT;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
