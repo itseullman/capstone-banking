54a20745fee28c231723c01a882deda2fac843b9
itseullman <eullman@kent.edu>
2021-11-28 23:17:29 -0500

    Update Biography - Contribution References - References properly listed out with their respective numbers - Updated any references to any internal pages - Added comments

--- scripts/biography.js
+++ scripts/biography.js
@@ -4,12 +4,16 @@ const tabContents = document.querySelectorAll('[data-tab-content]')
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
+
+        // remove 'active' from all tabs and all content
         tabContents.forEach(tabContent => {
             tabContent.classList.remove('active')
         })
         tabs.forEach(tab => {
             tab.classList.remove('active')
         })
+
+        // add 'active' to selected tab and its associated content
         tab.classList.add('active')
         target.classList.add('active')
     })
